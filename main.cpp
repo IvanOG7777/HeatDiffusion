9175f7881d3e621196589b1ff9335195c25f540c
Ivan <elderivanargueta04@gmail.com>
2026-08-18 14:33:46 -0700

    Fixed end cells being 0

--- main.cpp
+++ main.cpp
@@ -5,19 +5,22 @@
 #include <iostream>
 
 constexpr float DX = 1;
-constexpr float DT = 0.016;
+constexpr float DT = 0.1;
 constexpr int CELL_SIZE = 20;
 
 void calculateNewTemp(float *cells) {
     float newCells[CELL_SIZE] = {};
     for (int i = 0; i < CELL_SIZE; i++) {
         if (i == 0 || i == CELL_SIZE-1) continue;
-        float newTemp = cells[i] + 0.1f  * (cells[i - 1] - 2*cells[i] + cells[i + 1]);
+        float partialNewTemp = cells[i] + DT  * (cells[i - 1] - 2*cells[i] + cells[i + 1]);
+
+        float newTemp = partialNewTemp / (DX * DX);
 
         newCells[i] = newTemp;
     }
 
     for (int i = 0; i < CELL_SIZE; i++) {
+        if (i == 0 || i == CELL_SIZE - 1) continue;
         cells[i] = newCells[i];
     }
 }
@@ -32,11 +35,12 @@ int main() {
     cells[CELL_SIZE/2] = 100.0f;
 
     for (int i = 0; i < CELL_SIZE; i++) {
-        std:: cout << cells[i] << " ";
+        printf("%.2f ", cells[i]);
     }
     std:: cout << std:: endl;
 
     for (int i = 0; i < 1000; i++) {
+        if (cells[CELL_SIZE / 2] <= 20.0f) break;
         calculateNewTemp(cells);
 
         for (int i = 0; i < CELL_SIZE; i++) {
@@ -44,7 +48,6 @@ int main() {
         }
         std:: cout << std:: endl;
     }
-
 
     free(cells);
 
