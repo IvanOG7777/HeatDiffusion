7c404b3603483545d818b501fd4c02355de15694
Ivan <elderivanargueta04@gmail.com>
2026-08-19 12:00:49 -0700

    Close
    Need to fix indexing issue but almost there

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 #include <iostream>
 
 constexpr float DX = 1;
+constexpr float DY = 1;
 constexpr float DT = 0.1;
 constexpr int CELL_SIZE_W = 5;
 constexpr int CELL_SIZE_H = 5;
@@ -21,20 +22,44 @@ bool checkValidCell(int row, int col) {
 
 void calculateNewTemp(float *cells) {
     float newCells[CELL_SIZE_H * CELL_SIZE_W] = {};
+    int neighborCellCount[CELL_SIZE_H * CELL_SIZE_W] = {};
     for (int row = 0; row < CELL_SIZE_H; row++) {
         for (int col = 0; col < CELL_SIZE_W; col++) {
             int flatIndex = row * CELL_SIZE_W + col;
-
+            int neighborCount = 0;
+            int neighborCells[4] = {0};
+            int index = 0;
             for (auto &direction : directions) {
                 int x = row  + direction[0];
                 int y = row  + direction[1];
 
                 if (checkValidCell(x, y) == false) continue;
 
-                int innerFlatIndex = x * CELL_SIZE_W + col;
+                int innerFlatIndex = x * CELL_SIZE_W + y;
+                neighborCount++;
+                neighborCells[index++] = innerFlatIndex;
+            }
+            neighborCellCount[flatIndex] = neighborCount;
+            if (neighborCount == 4) {
+                float xAxisPartialSum = DT * (cells[neighborCells[0]] - (2*cells[flatIndex]) + cells[neighborCells[1]]);
+                float yAxisPartialSum = DT * (cells[neighborCells[1]] - (2*cells[flatIndex]) + cells[neighborCells[3]]);
 
-                float partialNewTemp = cells[innerFlatIndex] + DT * (cells[flatIndex - 1] - 2*cells[flatIndex] + cells[flatIndex + 1]);
-                float newTemp = partialNewTemp / (DX * DX);
+                float xAxisFinalSum = xAxisPartialSum / (DX * DX);
+                float yAxisFinalSum = yAxisPartialSum / (DY * DY);
+
+                float finalSum = cells[flatIndex] + xAxisFinalSum + yAxisFinalSum;
+
+                newCells[flatIndex] = finalSum;
+            }
+        }
+    }
+
+    for (int row = 0; row < CELL_SIZE_H; row++) {
+        for (int col = 0; col < CELL_SIZE_W; col++) {
+            int flatIndex = row * CELL_SIZE_W + col;
+
+            if (neighborCellCount[flatIndex] == 4) {
+                cells[flatIndex] = newCells[flatIndex];
             }
         }
     }
@@ -51,6 +76,18 @@ int main() {
     std:: cout << (CELL_SIZE_W * CELL_SIZE_H) / 2 << std:: endl;
 
     cells[(CELL_SIZE_W * CELL_SIZE_H)/2] = 100.0f;
+
+    for (int row = 0; row < CELL_SIZE_H; row++) {
+        for (int col = 0; col < CELL_SIZE_W; col++) {
+            int flatIndex = row * CELL_SIZE_W + col;
+
+            std:: cout << cells[flatIndex] << " ";
+        }
+        std:: cout << std:: endl;
+    }
+    std:: cout << std:: endl;
+
+    calculateNewTemp(cells);
 
     for (int row = 0; row < CELL_SIZE_H; row++) {
         for (int col = 0; col < CELL_SIZE_W; col++) {
