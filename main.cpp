7886aeb594c72688f3a41b1b2f2eb188ffadfcdb
Ivan <elderivanargueta04@gmail.com>
2026-08-19 10:28:33 -0700

    Trying to make this hoe 2d
    Gunna do this on my ipad rq

--- main.cpp
+++ main.cpp
@@ -6,48 +6,61 @@
 
 constexpr float DX = 1;
 constexpr float DT = 0.1;
-constexpr int CELL_SIZE = 20;
+constexpr int CELL_SIZE_W = 5;
+constexpr int CELL_SIZE_H = 5;
+
+int directions [4][2] = {
+    {-1, 0,},{1, 0}, // left, right
+    {0, -1}, {0, 1} // down, up
+};
+
+bool checkValidCell(int row, int col) {
+    if (row < 0 || row >= CELL_SIZE_H || col < 0 || col >= CELL_SIZE_W) return false;
+    return true;
+}
 
 void calculateNewTemp(float *cells) {
-    float newCells[CELL_SIZE] = {};
-    for (int i = 0; i < CELL_SIZE; i++) {
-        if (i == 0 || i == CELL_SIZE-1) continue;
-        float partialNewTemp = cells[i] + DT  * (cells[i - 1] - 2*cells[i] + cells[i + 1]);
+    float newCells[CELL_SIZE_H * CELL_SIZE_W] = {};
+    for (int row = 0; row < CELL_SIZE_H; row++) {
+        for (int col = 0; col < CELL_SIZE_W; col++) {
+            int flatIndex = row * CELL_SIZE_W + col;
 
-        float newTemp = partialNewTemp / (DX * DX);
+            for (auto &direction : directions) {
+                int x = row  + direction[0];
+                int y = row  + direction[1];
 
-        newCells[i] = newTemp;
-    }
+                if (checkValidCell(x, y) == false) continue;
 
-    for (int i = 0; i < CELL_SIZE; i++) {
-        if (i == 0 || i == CELL_SIZE - 1) continue;
-        cells[i] = newCells[i];
+                int innerFlatIndex = x * CELL_SIZE_W + col;
+
+                float partialNewTemp = cells[innerFlatIndex] + DT * (cells[flatIndex - 1] - 2*cells[flatIndex] + cells[flatIndex + 1]);
+                float newTemp = partialNewTemp / (DX * DX);
+            }
+        }
     }
 }
 
 int main() {
-    float *cells = static_cast<float *>(calloc(CELL_SIZE, sizeof(float)));
+    float *cells = static_cast<float *>(calloc(CELL_SIZE_W * CELL_SIZE_H, sizeof(float)));
 
-    for (int i = 0 ; i < CELL_SIZE;  i++) {
+    for (int i = 0 ; i < CELL_SIZE_W * CELL_SIZE_H;  i++) {
         cells[i] = 20.0f;
     }
 
-    cells[CELL_SIZE/2] = 100.0f;
+    std:: cout << CELL_SIZE_W * CELL_SIZE_H << std:: endl;
+    std:: cout << (CELL_SIZE_W * CELL_SIZE_H) / 2 << std:: endl;
 
-    for (int i = 0; i < CELL_SIZE; i++) {
-        printf("%.2f ", cells[i]);
-    }
-    std:: cout << std:: endl;
+    cells[(CELL_SIZE_W * CELL_SIZE_H)/2] = 100.0f;
 
-    for (int i = 0; i < 1000; i++) {
-        if (cells[CELL_SIZE / 2] <= 20.0f) break;
-        calculateNewTemp(cells);
+    for (int row = 0; row < CELL_SIZE_H; row++) {
+        for (int col = 0; col < CELL_SIZE_W; col++) {
+            int flatIndex = row * CELL_SIZE_W + col;
 
-        for (int i = 0; i < CELL_SIZE; i++) {
-            printf("%.2f ", cells[i]);
+            std:: cout << cells[flatIndex] << " ";
         }
         std:: cout << std:: endl;
     }
+    std:: cout << std:: endl;
 
     free(cells);
 
