a7efd4f8d3c4e5e4d1f6b9d143167556fd49278f
Ivan <elderivanargueta04@gmail.com>
2026-08-21 10:32:25 -0700

    Halo cells
    Figured out how to input halo cells to the shared memory. Skipping the corner cells since we only need to check left, right, up, down

--- Tiled/deviceFunctions.cu
+++ Tiled/deviceFunctions.cu
@@ -4,8 +4,6 @@
 
 #include "deviceFunctions.cuh"
 
-#include <float.h>
-
 __device__ bool kernelValidCell(int row, int col) {
     if (row < 0 || row >= CELL_SIZE_H || col < 0 || col >= CELL_SIZE_W) return false;
     return true;
@@ -22,17 +20,53 @@ __global__ void kernelCalculateCellTemp(float *cellsIn, float *cellsOut) {
     unsigned int localCol = threadIdx.x;
     unsigned int localRow = threadIdx.y;
 
+    unsigned int sharedRow = localRow + 1;
+    unsigned int sharedCol = localCol + 1;
+
+    int directions [4][2] = {
+        {-1, 0,},{1, 0}, // left, right
+        {0, -1}, {0, 1} // down, up
+    };
+
+    int neighborCount = 0;
+    int neighborCells[4] = {0};
+    int index = 0;
+
     __shared__ float sharedTemperatures[TPB + 2][TPB + 2] = {0.0f};
 
     //loads normal interior cells
     if (kernelValidCell(static_cast<int>(globalRow), static_cast<int>(globalCol)) == true) {
-        sharedTemperatures[localRow + 1][localCol + 1] = cellsIn[globalIndex];
+        sharedTemperatures[sharedRow][sharedCol] = cellsIn[globalIndex]; // thread in shared memory coordinates
     } else {
-        sharedTemperatures[localRow + 1][localCol + 1] = 0.0f;
+        sharedTemperatures[sharedRow][sharedCol] = 0.0f;
     }
 
+    // loads left col of halo cells
+    if (kernelValidCell(static_cast<int>(globalRow), static_cast<int>(globalCol) - 1) && localCol == 0) {
+        int neighborGlobalIndex = static_cast<int>(globalRow) * CELL_SIZE_W + (static_cast<int>(globalCol) - 1);
+        sharedTemperatures[sharedRow][0] = cellsIn[neighborGlobalIndex];
+    }
 
+    // loads right col of halo cells                                                                 // threads we have in current block in col direction (16)
+    if (kernelValidCell(static_cast<int>(globalRow), static_cast<int>(globalCol) + 1) && localCol == blockDim.x - 1) {
+        int neighborGlobalIndex = static_cast<int>(globalRow) * CELL_SIZE_W + (static_cast<int>(globalCol) + 1);
+        sharedTemperatures[sharedRow][blockDim.x + 1] = cellsIn[neighborGlobalIndex];
+    }
 
+    // loads top row of halo cells
+    if (kernelValidCell(static_cast<int>(globalRow) - 1, static_cast<int>(globalCol)) && localRow == 0) {
+        int neighborGlobalIndex = (static_cast<int>(globalRow) - 1) * CELL_SIZE_W + static_cast<int>(globalCol);
+        sharedTemperatures[0][sharedCol] = cellsIn[neighborGlobalIndex];
+    }
+
+    // loads bottom row of halo cells                                                                     // threads we have in current blocks in row direction (16)
+    if (kernelValidCell(static_cast<int>(globalRow) + 1, static_cast<int>(globalCol)) && localRow == blockDim.y - 1) {
+        int neighborGlobalIndex = (static_cast<int>(globalRow) + 1) * CELL_SIZE_W + static_cast<int>(globalCol);
+        sharedTemperatures[blockDim.y + 1][sharedCol] = cellsIn[neighborGlobalIndex];
+    }
     __syncthreads();
 
+    for (auto &direction : directions) {
+        
+    }
 }
